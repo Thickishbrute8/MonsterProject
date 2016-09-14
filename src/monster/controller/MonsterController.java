20f3953ad0145c0a8e065b23d44914aade66f8e5
Britton Perry <bper5656@740s-pl-132.local>
2016-09-14 08:21:46 -0600

    Initial setup

---
+++ src/monster/controller/MonsterController.java
@@ -0,0 +1,11 @@
+package monster.controller;
+
+public class MonsterController
+{
+
+	public static void main (String [] args)
+	{
+		MonsterController monsterAppController = new MonsterController();
+		monsterAppController.start();
+	}
+}
